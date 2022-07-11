01d47dc1e0783fb6ab512197d91c94b7ff66f2bc
Yourstruggle11 <sensouvik51@gmail.com>
2022-07-12 00:12:22 +0530

    `feat`: setup `firebase` for ``socail auth

---
+++ config/firebase.admin.config.js
@@ -0,0 +1,10 @@
+import admin from 'firebase-admin'
+
+import serviceAccount from './firebaseAdminSdk.js'
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'haveitdiscussed-11'
+})
+
+export default admin
