056c64a90e45fa1c48e44e3ab80ad427ac933ed6
Yourstruggle11 <sensouvik51@gmail.com>
2022-07-16 00:02:18 +0530

    `feat`: Added `comment` `model`

---
+++ model/comments.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+
+const commentSchema = new mongoose.Schema({
+    commentTitle: {
+        type: String,
+        required: true
+    },
+    commentBody: {
+        type: String,
+        required: true
+    },
+    commentedBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    question: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Question'
+    },
+    likeCount: {
+        type: Number,
+        default: 0
+      },
+      likedBy: [
+        {
+          type: mongoose.Types.ObjectId,
+          ref: 'User'
+        }
+      ],
+},
+{ timestamps: true }
+);
+
+
+const commentModel = mongoose.model('Comment', commentSchema);
+
+export default commentModel;
