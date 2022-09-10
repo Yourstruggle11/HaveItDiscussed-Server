9db19d72d612a75a1450998eacc414bbbeab5433
Yourstruggle11 <sensouvik51@gmail.com>
2022-09-11 02:02:35 +0530

    `fix`: Fixed `firebase` `privatekey` error

--- config/firebaseAdminSdk.js
+++ config/firebaseAdminSdk.js
@@ -5,7 +5,7 @@ export default {
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key: process.env.FIREBASE_PRIVATE_KEY ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY) : undefined,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
