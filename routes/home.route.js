e1c40794a0cf5448e2035b7743aaa75f677440f8
Yourstruggle11 <sensouvik51@gmail.com>
2022-09-11 01:52:10 +0530

    `[UPDATE]`: updated `entryPoint`

--- routes/home.route.js
+++ routes/home.route.js
@@ -6,7 +6,7 @@ const time = new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' })
 
 router.get('/', (req, res, next) => {
     return res.json({
-        message: 'InterviewReady Discussion Forum Server',
+        message: 'HaveItDiscussed Server is running...',
         deployTime: time,
         env: process.env.NODE_ENV
     })
