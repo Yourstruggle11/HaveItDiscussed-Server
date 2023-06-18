86aab826d7829bb49ffc353bd194bda6e3bb5d1f
Yourstruggle11 <sensouvik51@gmail.com>
2023-06-19 00:48:46 +0530

    `feat`: Modernized `Error Handling`

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,29 +1,26 @@
 import jwt from 'jsonwebtoken'
+import ErrorHandler from './ErrorClass.js';
 
 export const userRouteProtection = (req, res, next) => {
     // get auth token from header
-    const authHeader = req.headers.authorization
-    const authBody = req.body.token
-    let token
+    const authHeader = req.headers.authorization;
+    const authBody = req.body.token;
+    let token;
 
     if (authBody) {
         // get auth token from body
-        token = authBody
+        token = authBody;
     } else if (authHeader) {
         // get auth token from header
-        token = authHeader && authHeader.split(' ')[1]
+        token = authHeader && authHeader.split(' ')[1];
     } else {
-        const err = new Error('No token, authorization denied')
-        err.status = 404
-        throw err
+        throw ErrorHandler.notFoundError('No token present, authorization denied');
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
         if (err) {
-            const err = new Error('Token is invalid')
-            err.status = 403
-            throw err
+            throw ErrorHandler.forbidden('Token is invalid');
         }
-        req.user = user
-        next()
-    })
+        req.user = user;
+        next();
+    });
 }
